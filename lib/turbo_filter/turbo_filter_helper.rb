54c3225a3ba46e233956eeacd03d36da6df2c2c6
Sandeep Kumar <sandeep.kumar@payu.in>
2016-01-03 23:55:58 +0530

    dynamically build filters

--- lib/turbo_filter/turbo_filter_helper.rb
+++ lib/turbo_filter/turbo_filter_helper.rb
@@ -1,90 +1,81 @@
 module ActionView
   module Helpers
-  	module TurboFilterHelper
+    module TurboFilterHelper
 
-  		def turbo_filters_for s  			
-  			tfq = TurboFilterQuery.new
-	      tfq.add_available_filter "status_id",
-	      :type => :list_optional, :values => MerchantStatus.sorted.collect{|s| [s.name, s.id.to_s] }, :name => 'Status'
+      def turbo_filters_for filters_for_class=nil
+        tfq = TurboFilterQuery.new(filters_for_class)
+        render :partial => 'turbo_filters/filters', :layout => false, :locals => {:query => tfq}
+      end
 
-	      tfq.add_available_filter "tracker_id",
-	        :type => :list, :values => Tracker.all.collect{ |s| [s.name, s.id.to_s] }
+      def include_calendar_headers_tags
+        unless @calendar_headers_tags_included
+          tags = javascript_include_tag("datepicker")
+          @calendar_headers_tags_included = true
+          content_for :header_tags do
+            start_of_week = Setting.start_of_week
+            start_of_week = l(:general_first_day_of_week, :default => '1') if start_of_week.blank?
+            # TurboFilter uses 1..7 (monday..sunday) in settings and locales
+            # JQuery uses 0..6 (sunday..saturday), 7 needs to be changed to 0
+            start_of_week = start_of_week.to_i % 7
+            tags << javascript_tag(
+                       "var datepickerOptions={dateFormat: 'yy-mm-dd', firstDay: #{start_of_week}, " +
+                         "showOn: 'button', buttonImageOnly: true, buttonImage: '" +
+                         path_to_image('/images/calendar.png') +
+                         "', showButtonPanel: true, showWeek: true, showOtherMonths: true, " +
+                         "selectOtherMonths: true, changeMonth: true, changeYear: true, " +
+                         "beforeShow: beforeShowDatePicker};")
+            jquery_locale = l('jquery.locale', :default => current_language.to_s)
+            unless jquery_locale == 'en'
+              tags << javascript_include_tag("i18n/datepicker-#{jquery_locale}.js")
+            end
+            tags
+          end
+        end
+      end
 
-	      tfq.add_available_filter "priority_id",
-	        :type => :list, :values => MerchantPriority.all.collect{|s| [s.name, s.id.to_s] }
+      def filters_options_for_select(query)
+        options_for_select(filters_options(query))
+      end
 
-  			render :partial => 'turbo_filters/filters', :layout => false, :locals => {:query => tfq}
-  		end
+      def filters_options(query)
+        options = [[]]
+        options += query.available_filters.map do |field, field_options|
+          [field_options[:name], field]
+        end
+      end
 
-  		def include_calendar_headers_tags
-		    unless @calendar_headers_tags_included
-		      tags = javascript_include_tag("datepicker")
-		      @calendar_headers_tags_included = true
-		      content_for :header_tags do
-		        start_of_week = Setting.start_of_week
-		        start_of_week = l(:general_first_day_of_week, :default => '1') if start_of_week.blank?
-		        # TurboFilter uses 1..7 (monday..sunday) in settings and locales
-		        # JQuery uses 0..6 (sunday..saturday), 7 needs to be changed to 0
-		        start_of_week = start_of_week.to_i % 7
-		        tags << javascript_tag(
-		                   "var datepickerOptions={dateFormat: 'yy-mm-dd', firstDay: #{start_of_week}, " +
-		                     "showOn: 'button', buttonImageOnly: true, buttonImage: '" +
-		                     path_to_image('/images/calendar.png') +
-		                     "', showButtonPanel: true, showWeek: true, showOtherMonths: true, " +
-		                     "selectOtherMonths: true, changeMonth: true, changeYear: true, " +
-		                     "beforeShow: beforeShowDatePicker};")
-		        jquery_locale = l('jquery.locale', :default => current_language.to_s)
-		        unless jquery_locale == 'en'
-		          tags << javascript_include_tag("i18n/datepicker-#{jquery_locale}.js")
-		        end
-		        tags
-		      end
-		    end
-		  end
+      def query_filters_hidden_tags(query)
+        tags = ''.html_safe
+        query.filters.each do |field, options|
+          tags << hidden_field_tag("f[]", field, :id => nil)
+          tags << hidden_field_tag("op[#{field}]", options[:operator], :id => nil)
+          options[:values].each do |value|
+            tags << hidden_field_tag("v[#{field}][]", value, :id => nil)
+          end
+        end
+        tags
+      end
 
-		  def filters_options_for_select(query)
-		    options_for_select(filters_options(query))
-		  end
+      def query_columns_hidden_tags(query)
+        tags = ''.html_safe
+        query.columns.each do |column|
+          tags << hidden_field_tag("c[]", column.name, :id => nil)
+        end
+        tags
+      end
 
-		  def filters_options(query)
-		    options = [[]]
-		    options += query.available_filters.map do |field, field_options|
-		      [field_options[:name], field]
-		    end
-		  end
+      def query_hidden_tags(query)
+        query_filters_hidden_tags(query) + query_columns_hidden_tags(query)
+      end
 
-		  def query_filters_hidden_tags(query)
-		    tags = ''.html_safe
-		    query.filters.each do |field, options|
-		      tags << hidden_field_tag("f[]", field, :id => nil)
-		      tags << hidden_field_tag("op[#{field}]", options[:operator], :id => nil)
-		      options[:values].each do |value|
-		        tags << hidden_field_tag("v[#{field}][]", value, :id => nil)
-		      end
-		    end
-		    tags
-		  end
+      def available_block_columns_tags(query)
+        tags = ''.html_safe
+        query.available_block_columns.each do |column|
+          tags << content_tag('label', check_box_tag('c[]', column.name.to_s, query.has_column?(column), :id => nil) + " #{column.caption}", :class => 'inline')
+        end
+        tags
+      end
 
-		  def query_columns_hidden_tags(query)
-		    tags = ''.html_safe
-		    query.columns.each do |column|
-		      tags << hidden_field_tag("c[]", column.name, :id => nil)
-		    end
-		    tags
-		  end
-
-		  def query_hidden_tags(query)
-		    query_filters_hidden_tags(query) + query_columns_hidden_tags(query)
-		  end
-
-		  def available_block_columns_tags(query)
-		    tags = ''.html_safe
-		    query.available_block_columns.each do |column|
-		      tags << content_tag('label', check_box_tag('c[]', column.name.to_s, query.has_column?(column), :id => nil) + " #{column.caption}", :class => 'inline')
-		    end
-		    tags
-		  end
-
-  	end
+    end
   end
 end
--- lib/turbo_filter/turbo_filter_query.rb
+++ lib/turbo_filter/turbo_filter_query.rb
@@ -1,57 +1,86 @@
 class TurboFilterQuery
-	def self.included(base)
-		base.extend ClassMethods			
-	end
+  def self.included(base)
+    base.extend ClassMethods
+  end
 
-	  class_attribute :operators
-	  self.operators = {
-	    "="   => :label_equals,
-	    "!"   => :label_not_equals,
-	    "!*"  => :label_none,
-	    "*"   => :label_any,
-	    ">="  => :label_greater_or_equal,
-	    "<="  => :label_less_or_equal,
-	    "><"  => :label_between,
-	    "<t+" => :label_in_less_than,
-	    ">t+" => :label_in_more_than,
-	    "><t+"=> :label_in_the_next_days,
-	    "t+"  => :label_in,
-	    "t"   => :label_today,
-	    "ld"  => :label_yesterday,
-	    "w"   => :label_this_week,
-	    "lw"  => :label_last_week,
-	    "l2w" => [:label_last_n_weeks, {:count => 2}],
-	    "m"   => :label_this_month,
-	    "lm"  => :label_last_month,
-	    "y"   => :label_this_year,
-	    ">t-" => :label_less_than_ago,
-	    "<t-" => :label_more_than_ago,
-	    "><t-"=> :label_in_the_past_days,
-	    "t-"  => :label_ago,
-	    "~"   => :label_contains,
-	    "!~"  => :label_not_contains
-	  }
+    class_attribute :operators
+    self.operators = {
+      "="   => :label_equals,
+      "!"   => :label_not_equals,
+      "!*"  => :label_none,
+      "*"   => :label_any,
+      ">="  => :label_greater_or_equal,
+      "<="  => :label_less_or_equal,
+      "><"  => :label_between,
+      "<t+" => :label_in_less_than,
+      ">t+" => :label_in_more_than,
+      "><t+"=> :label_in_the_next_days,
+      "t+"  => :label_in,
+      "t"   => :label_today,
+      "ld"  => :label_yesterday,
+      "w"   => :label_this_week,
+      "lw"  => :label_last_week,
+      "l2w" => [:label_last_n_weeks, {:count => 2}],
+      "m"   => :label_this_month,
+      "lm"  => :label_last_month,
+      "y"   => :label_this_year,
+      ">t-" => :label_less_than_ago,
+      "<t-" => :label_more_than_ago,
+      "><t-"=> :label_in_the_past_days,
+      "t-"  => :label_ago,
+      "~"   => :label_contains,
+      "!~"  => :label_not_contains
+    }
 
-  	class_attribute :operators_by_filter_type
-	  self.operators_by_filter_type = {
-	    :list => [ "=", "!" ],		    
-	    :list_optional => [ "=", "!", "!*", "*" ],		    
-	    :date => [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ],
-	    :date_past => [ "=", ">=", "<=", "><", ">t-", "<t-", "><t-", "t-", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", "!*", "*" ],
-	    :string => [ "=", "~", "!", "!~", "!*", "*" ],
-	    :text => [  "~", "!~", "!*", "*" ],
-	    :integer => [ "=", ">=", "<=", "><", "!*", "*" ],
-	    :float => [ "=", ">=", "<=", "><", "!*", "*" ]		    
-	  }
+    class_attribute :operators_by_filter_type
+    self.operators_by_filter_type = {
+      :list => [ "=", "!" ],
+      :list_optional => [ "=", "!", "!*", "*" ],
+      :date => [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ],
+      :date_past => [ "=", ">=", "<=", "><", ">t-", "<t-", "><t-", "t-", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", "!*", "*" ],
+      :string => [ "=", "~", "!", "!~", "!*", "*" ],
+      :text => [  "~", "!~", "!*", "*" ],
+      :integer => [ "=", ">=", "<=", "><", "!*", "*" ],
+      :float => [ "=", ">=", "<=", "><", "!*", "*" ]
+    }
 
-	# Returns a hash of localized labels for all filter operators
+  # Returns a hash of localized labels for all filter operators
   def self.operators_labels
     operators.inject({}) {|h, operator| h[operator.first] = *operator.last; h}
   end
 
+  def initialize(filters_for_class=nil)
+    @filters_for_class = filters_for_class
+  end
+
   # Adds available filters
   def initialize_available_filters
-    # implemented by sub-classes
+    if @filters_for_class.is_a?(Class) && (@filters_for_class.superclass == ActiveRecord::Base)
+      associations = @filters_for_class.reflect_on_all_associations(:belongs_to)
+      association_foreign_keys = associations.map(&:foreign_key)
+      @filters_for_class.columns.each do |col|
+        case col.type
+        when :string
+          add_available_filter col.name, :type => :text
+        when :date
+          add_available_filter col.name, :type => :date
+        when :datetime
+          add_available_filter col.name, :type => :date_past
+        when :float
+          add_available_filter col.name, :type => :float
+        when :integer
+          if association_foreign_keys.include?(col.name)
+            association_class = associations.select { |a| a.foreign_key == col.name }.first.class_name.constantize
+            association_values = association_class.all.collect{|s| [s.try(:name), s.id.to_s] }
+            add_available_filter col.name, :type => :list, :values => association_values
+          else
+            add_available_filter col.name, :type => :integer
+          end
+        when :boolean
+          add_available_filter col.name, :type => :list, :values => [["Yes","1"],["No", "0"]]
+        end # case
+      end # do
+    end
   end
   protected :initialize_available_filters
 
@@ -73,7 +102,7 @@ class TurboFilterQuery
   def available_filters
     unless @available_filters
       initialize_available_filters
-      @available_filters.each do |field, options|
+      @available_filters.to_a.each do |field, options|
         options[:name] ||= options[:label] || "field_#{field}".gsub(/_id$/, '')
       end
     end
@@ -137,11 +166,11 @@ class TurboFilterQuery
   # Returns a representation of the available filters for JSON serialization
   def available_filters_as_json
     json = {}
-    available_filters.each do |field, options|
+    available_filters.to_a.each do |field, options|
       json[field] = options.slice(:type, :name, :values).stringify_keys
     end
     json
-  end  
+  end
 
   # Helper method to generate the WHERE sql for a +field+, +operator+ and a +value+
   def sql_for_field(field, operator, value, db_table, db_field, is_custom_filter=false)
@@ -213,7 +242,7 @@ class TurboFilterQuery
         else
           sql = "#{db_table}.#{db_field} BETWEEN #{value[0].to_f} AND #{value[1].to_f}"
         end
-      end	    
+      end
     when "><t-"
       # between today - n days and today
       sql = relative_date_clause(db_table, db_field, - value.first.to_i, 0)
